83efaf1f226d47cb1281eb57cec895143f840b59
paulo maia <pauloafmaia9@gmail.com>
2022-12-09 21:11:56 -0300

    First Commit

---
+++ Blood-Center/src/main/java/application/Blood/Center/BloodCenterApplication.java
@@ -0,0 +1,13 @@
+package application.Blood.Center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodCenterApplication.class, args);
+	}
+
+}
---
+++ Blood-Center/src/test/java/application/Blood/Center/BloodCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package application.Blood.Center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
